68c171f48dcfa35ad53c1bb2db5a943282aade45
velykitty <jiyunee13@gmail.com>
2020-12-15 01:19:56 +0900

    background gradient -import LinearGradient

--- Weather.js
+++ Weather.js
@@ -1,20 +1,65 @@
 import React from "react";
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, View, StatusBar} from "react-native";
 import PropTypes from "prop-types";
+import {LinearGradient} from 'expo-linear-gradient';
 import {MaterialCommunityIcons} from "@expo/vector-icons";
 //https://docs.expo.io/guides/icons/#expovector-icons
 
+const weatherOptions ={
+    Clear: {
+        iconName: "white-balance-sunny",
+        gradient: ["#1FA2FF", "#12D8FA", "#A6FFCB"]
+
+    },
+
+    Thunderstorm: {
+        iconName: "weather-lightning-rainy",
+        gradient: ["#1e130c", "#9a8478"]
+    },
+
+    Drizzle: {
+        iconName:"weather-partly-rainy", 
+        gradient: ["#DAD299", "#B0DAB9"]
+    },
+
+    Rain: {
+        iconName: "weather-rainy", 
+        gradient: ["#525252", "#3d72b4"]
+    },
+
+    Snow: {
+        iconName: "snowflake", 
+        gradient: ["#DAE2F8", "#D6A4A4"]
+    },
+
+    Atmosphere: {
+        iconName: "weather-fog", 
+        gradient: ["#544a7d", "#ffd452"]
+    },
+
+    Clouds: {
+        iconName: "cloud", 
+        gradient: ["#aa4b6b", "#6b6b83", "#3b8d99"]
+    }
+}
+
 export default function Weather({temp, condition}) {
     return (
-    <View style={styles.container}>
+    <LinearGradient
+    colors={weatherOptions[condition].gradient}
+    style={styles.container}>
+        <StatusBar barStyle="light-content" />
         <View style={styles.halfContainer}>
-            <MaterialCommunityIcons name="weather-lightning-rainy" size={96}></MaterialCommunityIcons>
-            <Text style={styles.temp}>{temp}</Text>
-            <Text>{condition}</Text>
+            <MaterialCommunityIcons
+            name={weatherOptions[condition].iconName}
+            size={96}
+            color="white" />
+            <Text style={styles.temp}>{temp}°</Text>
+            <Text style={styles.condition}>{condition}</Text>
         </View>
         <View style={styles.halfContainer}>
         </View>
-    </View>
+    </LinearGradient>
     );
 }
 
@@ -41,6 +86,12 @@ const styles = StyleSheet.create({
 
     temp: {
         fontSize: 42,
+        color: "white",
+    },
+
+    condition: {
+        fontSize: 20,
+        color: "white",
     },
 
     halfContainer: {
